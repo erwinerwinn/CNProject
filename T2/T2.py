7f872f7db9faec5763a30d583883918d8148fdfc
erwinerwinn <finderwinhere@gmail.com>
2018-11-22 18:47:11 +0800

    asdasdasd

--- T2/T2.py
+++ T2/T2.py
@@ -6,12 +6,12 @@ counter = 0
 with open('TLD.dictionary', 'r') as f:
     for line in f:
         fields = line.split(" ")
-        print(fields[1])
-
-        p = subprocess.Popen(["dig", fields[1], "NS"], stdout=subprocess.PIPE)
+        #p = subprocess.Popen(["dig", fields[1].rstrip(), "+dnssec"], stdout=subprocess.PIPE)
+        p = subprocess.Popen(["dig", "+noall", "+answer", "-x", fields[1].rstrip(), "+dnssec"], stdout=subprocess.PIPE)
         resultString = p.communicate()[0].decode('UTF-8')
-
-
+        print(resultString)
+        domainName = resultString.split(" ")
+        print(domainName[1])
 
 # run The above with wireshark opened
 
