4a982df35f2c0ade3a382858a7ca803403d73379
erwinerwinn <finderwinhere@gmail.com>
2018-11-23 10:57:41 +0800

    asdasdasd

--- T2/T2-2.py
+++ T2/T2-2.py
@@ -23,4 +23,4 @@ with open('/Users/erwinerwinn/Desktop/MSSD/Computer Networks/CNProjects/T2/T2-An
 
     print("Top 10 dst with Highest Amplification Factor")
     for k, v in sorted(afDict.items(), key=lambda kv: kv[1], reverse= True)[:10]:
-        print("Destination IP: ", k, "has amplification factor of: ", v[0], " Request sent: ",v[2], " response received: ", v[1])
+        print("Destination IP: ", k, "has amplification factor of: ", v[0], " Request sent: ",v[2], "B response received: ", v[1],"B")
