13e9a45fb978d427888cbe42985e8edfce10bfca
erwinerwinn <finderwinhere@gmail.com>
2018-11-23 10:15:39 +0800

    asdasdasd

---
+++ T2/T2-2.py
@@ -0,0 +1,26 @@
+import re
+
+
+topAF = [5]
+afDict = {}
+
+with open('/Users/erwinerwinn/Desktop/MSSD/Computer Networks/CNProjects/T2/T2-Analysis.txt', 'r') as f:
+    for line in f:
+        fields = line.split()
+        port = re.search('(?<=:).*$', fields[2]).group(0)
+        srcIP = fields[0]
+        dstIP = fields[2]
+        requestSize = int(fields[6])
+        responseSize = int(fields[4])
+
+        if ( port == "53"):
+            if(dstIP not in afDict.keys()):
+                try:
+                    amplificationFactor = responseSize / requestSize
+                except:
+                    amplificationFactor = 0
+                afDict[dstIP] = amplificationFactor
+
+    print("Top 10 dst with Highest Amplification Factor")
+    for k, v in sorted(afDict.items(), key=lambda kv: kv[1], reverse= True)[:10]:
+        print(k, v)
