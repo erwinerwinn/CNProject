ceb189bb32fb3847dcf653890b520c8242952744
kseet001 <kseet001@gmail.com>
2018-12-02 21:39:19 +0800

    Minor modification

--- T3/T3.py
+++ T3/T3.py
@@ -8,25 +8,21 @@ time = datetime.datetime.now()
 timestamp = time.strftime("%Y-%m-%d-%H:%M")
 output_filename = "output." + timestamp +".txt"
 output_file = open(output_filename, "w")
+sent = []
 
 with open('TLD.dictionary', 'r') as f:
     for line in f:
         fields = line.split(" ")
-        for i in range(500):
-            print(i+1)
-            output_file.write("Packet %s" %(i+1))
-            query_str = "dig %s @%s ANY +recurse +dnssec +bufsize=65535 +recurse +retry=0 +time=2" % (fields[0].rstrip(), fields[1].rstrip())
-            proc = subprocess.Popen(query_str, shell=True,
-                                    stdin=None, stdout=None, stderr=None, close_fds=True)
 
-        print("Going to sleep to free up resources")
-        now = datetime.datetime.now()
-        future = now + datetime.timedelta(seconds=5)
-        while datetime.datetime.now() < future:
-            # do nothing
-            pass
-        #for p in processes:
-        #    p.kill()
+        if fields[1].rstrip() not in sent:
+            sent.append(fields[1].rstrip())
+            for i in range(500):
+                print(i + 1)
+                output_file.write("Packet %s" % (i + 1))
+                query_str = "dig %s @%s ANY +recurse +dnssec +bufsize=65535 +recurse +retry=0 +time=3" % (
+                fields[0].rstrip(), fields[1].rstrip())
+                proc = subprocess.Popen(query_str, shell=True,
+                                        stdin=None, stdout=None, stderr=None, close_fds=True)
 
 # run The above with wireshark opened
 
