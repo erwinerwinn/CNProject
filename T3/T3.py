48c97879b7e895f6b44985b084f39a0b86494121
root <root@localhost.localdomain>
2018-12-03 00:06:20 +0800

    modified notcp

--- T3/T3.py
+++ T3/T3.py
@@ -15,7 +15,7 @@ with open('TLD.dictionary', 'r') as f:
         for i in range(500):
             print(i+1)
             output_file.write("Packet %s" %(i+1))
-            query_str = "dig %s @%s ANY +recurse +dnssec +bufsize=65535 +recurse +retry=0 +time=2" % (fields[0].rstrip(), fields[1].rstrip())
+            query_str = "dig %s @%s ANY +recurse +dnssec +bufsize=65535 +recurse +retry=0 +time=3 +notcp" % (fields[0].rstrip(), fields[1].rstrip())
             proc = subprocess.Popen(query_str, shell=True,
                                     stdin=None, stdout=None, stderr=None, close_fds=True)
 
